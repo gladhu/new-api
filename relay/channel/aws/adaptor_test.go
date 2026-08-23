787a84f431ce08767a7908625bf6436ca3bfe280
gladhu <gladhu2016@qq.com>
2026-08-23 23:36:09 +0800

    fix : compile error

--- relay/channel/aws/adaptor_test.go
+++ relay/channel/aws/adaptor_test.go
@@ -6,8 +6,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/QuantumNous/new-api/dto"
 	relaycommon "github.com/QuantumNous/new-api/relay/common"
+	"github.com/QuantumNous/new-api/relaykit/dto"
 	relayconstant "github.com/QuantumNous/new-api/relay/constant"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
